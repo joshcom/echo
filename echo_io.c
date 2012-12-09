59001e1cbbdeb20e0a8b379c141092b02fc3bb6e
Joshua Murray <joshua.murray@gmail.com>
2012-12-09 16:56:48 -0600

    Aaaand, echo it back.

--- echo_io.c
+++ echo_io.c
@@ -1,16 +1,16 @@
 #include "echo_io.h"
 
-ssize_t read_client_input(int socket, void *buffer_ptr, size_t max_length) {
+ssize_t read_client_input(int socket, void *buffer, size_t max_length) {
     ssize_t n, read_character;
-    char    c, *buffer;
+    char    c, *buffer_ptr;
 
-    buffer = buffer_ptr;
+    buffer_ptr = buffer;
 
     for ( n = 1; n < max_length; n++ ) {
 		read_character = read(socket, &c, 1);
 		if (read_character == 1) {
-			*buffer = c;
-			buffer++;
+			*buffer_ptr = c;
+			buffer_ptr++;
 			if (c == '\n') {
 				break;
 			}
@@ -33,10 +33,33 @@ ssize_t read_client_input(int socket, void *buffer_ptr, size_t max_length) {
 		}
 	}
 	
-	*buffer = 0;
+	*buffer_ptr = 0;
 	return 0;
 }
 
-ssize_t echo_to_client(int socket, const void *buffer_ptr, size_t max_length) {
-	return 0;
+ssize_t echo_to_client(int socket, const void *buffer, size_t max_length) {
+	const char *buffer_ptr;
+	size_t n;
+	ssize_t n_written;
+
+	buffer_ptr = buffer;
+	n = max_length;
+
+	while (n > 0) {
+		n_written = write(socket, buffer_ptr, n);
+		if (n_written <= 0) {
+			/* If we were interrupted, continue.  Otherwise, return an error. */
+			if ( errno == EINTR ) {
+				n_written = 0;
+			}
+			else {
+				return -1;
+			}
+		}
+		/* Account for what we've written, and shift the pointer. */
+		n -= n_written;
+		buffer_ptr += n_written;
+	}
+
+	return n_written;
 }
